c6538d89b8ee6ee96037ae01a03fdece242047ed
정성일 <jeongseong-il@jeongseong-il-ui-MacBookPro.local>
2022-05-24 16:48:42 +0900

    오류투성이

---
+++ frontUI/main.js
@@ -0,0 +1,65 @@
+
+
+function 다이추가(){
+
+    let 타겟=document.querySelector(".프레임")
+    let 다이= document.createElement('div');
+    let 다이수찾기=document.querySelectorAll(".다이")
+    let 다이수 = 다이수찾기.length+1
+    다이.setAttribute('class','다이')
+    let 타이머 = document.createElement('button')
+    타이머.setAttribute('id','타이머')
+    타이머.onclick=function(){타이머()}
+    다이.innerHTML = 다이수+"번 다이"
+    다이.appendChild(타이머)
+    
+    타겟.append(다이);
+}
+
+function 다이삭제(){
+    let 타겟=document.querySelector(".프레임")
+    let 타겟다이=document.querySelectorAll(".다이")
+    let 마지막 = 타겟다이.length-1
+    타겟.removeChild(타겟다이[마지막]);
+}
+
+let time = 0;
+
+function 타이머(){
+    let 타이머상태 = 'off'
+
+    if (타이머상태==='off') {
+        // 온으로 바뀌고 켜지게 만들기
+        타이머상태 = 'on'
+        let 다이=document.querySelector(".다이")
+        let 타이머박스= document.createElement('div');
+        타이머박스.setAttribute('class','타이머박스')
+        다이.appendChild(타이머박스)
+        시간표시=setInterval(업데이트,1000)
+        
+    } else if (타이머상태 ==='on'){
+        타이머상태 = 'off'
+        clearInterval(시간표시)
+    }
+}
+
+function 업데이트(){
+    const hours = Math.floor(time/3600);
+    const checkMinute = Math.floor(time/60);
+    const seconds = time%60;
+    const minute = checkMinute%60;
+
+    let 타이머박스=document.querySelector(".타이머박스")
+    타이머박스.innerHTML = `${hours}:${minute}:${seconds}`
+}
+
+
+function 결제하기(){
+    let 요금방식 = 
+    let 타이머박스=document.querySelector(".타이머박스")
+    let 시간=타이머박스.time
+    if (요금방식 ='카드할인')
+    
+    alert()
+    타이머박스.innerHTML = `${hours}:${minute}:${seconds}`
+}
---
+++ frontUI/salse.js
@@ -0,0 +1,41 @@
+
+
+function 조회(){
+    const 조회기준 = document.querySelector(".매출")
+    if(조회기준.innerHTML ==='년도별'){
+        let 년도별조회= document.createElement('select')
+        년도별조회.setAttribute('class','년도별')
+        조회기준.appendChild(년도별조회)
+        for (let year = 2018; year <= 2022; year++){
+            const 년도 = document.querySelector(".년도별")
+            const 년도옵션=document.createElement('option')
+            년도옵션.setAttribute('value',year)
+            년도옵션.innerHTML=year
+            년도.appendChild(년도옵션)
+        }
+    }else if(조회기준.innerHTML ==='월별'){
+        let 월별조회= document.createElement('select')
+        월별조회.setAttribute('class','월별')
+        조회기준.appendChild(월별조회)
+        for (let month = 1; month <= 12; month++){
+            const 당월 = document.querySelector(".월별")
+            const 월별옵션=document.createElement('option')
+            월별옵션.setAttribute('value',month)
+            월별옵션.innerHTML=month
+            당월.appendChild(월별옵션)
+        }
+
+    }else if(조회기준.innerHTML ==='일별'){
+        let 일별조회= document.createElement('select')
+        일별조회.setAttribute('class','일별')
+        조회기준.appendChild(일별조회)
+        for (let day = 1; day <= 31; day++){
+            const 일별 = document.querySelector(".일별")
+            const 일별옵션=document.createElement('option')
+            일별옵션.setAttribute('value',day)
+            일별옵션.innerHTML=day
+            일별.appendChild(일별옵션)
+        }
+
+
+}
